430f78e9be97a96dee532d4b085eca18389f9b15
Alexey Nikipelau <alexey.nikipelau@form.io>
2021-11-13 20:28:33 +0300

    /version endpoint added

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const config = require('./src/config');
 
+const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
@@ -9,9 +10,14 @@ const yaml = require('yamljs');
 const swaggerDocs = yaml.load('./docs/openapi/spec.yaml');
 const app = express();
 
+const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
+
 const pdfRouter = require('./src/routers/pdf-router');
 
 app.use(cors());
+app.get('/version', (req, res) => {
+  res.send({version});
+})
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/pdf', pdfRouter);
