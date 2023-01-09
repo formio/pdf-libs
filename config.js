d905cdb825faae4a6da6bfde2aa6e481cf99081e
brendanjbond <brendanbond@gmail.com>
2023-01-09 10:11:43 -0600

    move html gen timeout config to object

--- config.js
+++ config.js
@@ -29,11 +29,13 @@ if (!extractFormfieldsPath) {
 
 const hideFormfieldsPath = process.env.HIDE_FORMFIELDS;
 
-// 7 seconds seems like a sensible default I guess? We don't want PDFs that don't need postscript
-// optimization to be optimized in this way, but we also want to stay under common reverse proxy
-// timeout configurations so we don't receive a 504
-const htmlGenerationTimeout = process.env.HTML_GENERATION_TIMEOUT
-  ? process.env.HTML_GENERATION_TIMEOUT : 7000;
+// 7 seconds (and then 30 seconds) seems like a sensible default I guess? We don't want PDFs
+// that don't need postscript optimization to be optimized in this way, but we also want to
+// stay under common reverse proxy timeout configurations so we don't receive a 504
+const htmlGenerationTimeoutConfig = {
+  timeout: process.env.HTML_GENERATION_TIMEOUT ? process.env.HTML_GENERATION_TIMEOUT : 7000,
+  backoff: process.env.HTML_GENERATION_BACKOFF ? process.env.HTML_GENERATION_BACKOFF : 23000
+};
 
 module.exports = {
   port,
@@ -41,5 +43,5 @@ module.exports = {
   pdf2htmlexPath,
   psToPdfPath,
   hideFormfieldsPath,
-  htmlGenerationTimeout
+  htmlGenerationTimeoutConfig
 };
